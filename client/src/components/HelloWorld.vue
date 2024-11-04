05fcd2f0a24389c9434717825b0a194ba3cfbd45
therealnaveennb <therealnaveennb@gmail.com>
2024-11-04 15:21:27 +0530

    client added

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        message: 'Hello, Vue!'
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .hello {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    color: #2c3e50;
+    margin-top: 40px;
+  }
+  </style>
+  
